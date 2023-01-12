359d7c8cca82d1fb25691f34ed028fd0730e4599
Lucas Barboza Costa <122161095+Lucasbc47@users.noreply.github.com>
2023-01-12 11:58:52 -0300

    Add files via upload

---
+++ exercicio1.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+import java.util.Arrays;
+public class exercicio1 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Insira o primeiro numero:");
+        int num1 = input.nextInt();
+
+        System.out.println("Insira o segundo numero:");
+        int num2 = input.nextInt();
+
+        System.out.println("Insira o terceiro numero:");
+        int num3 = input.nextInt();
+
+        int soma = num1 + num2 + num3;
+        System.out.println("Soma: " + soma);
+
+        int arr[] = {num1, num2, num3};
+        Arrays.sort(arr);
+        System.out.println("Maior numero: " + arr[2]);
+    }
+}
