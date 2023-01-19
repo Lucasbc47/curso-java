6ce312914018a37baaf431fe36921a1bfd3b0039
Lucas Barboza Costa <122161095+Lucasbc47@users.noreply.github.com>
2023-01-19 13:20:43 -0300

    Update Main.java

--- 006/Main.java
+++ 006/Main.java
@@ -1,18 +1,16 @@
-import java.util.Scanner;
-
 public class Main {
     public static void main(String[] args) {
 
-        Scanner entrada = new Scanner(System.in);
         double aumento = 1.5, salario = 1000, ano = 2022;
 
         do {
-            aumento = aumento * 2;
-            salario = salario + (salario + aumento / 100);
-            ano++;
+            aumento = aumento * 2; // 1.5% * 2
+            salario = salario + (salario + aumento / 100); // 1000 + 1.5%
+            ano++; // Incrementa +1
         }
 
         while (ano <= 2022); {
+            // Enquanto for menor ou igual a 2022:
             System.out.println(salario);
         }
 
