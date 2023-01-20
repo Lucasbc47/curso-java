b71a265b232dcf792b6cd1bc1ba6466053e9559c
Lucas Barboza Costa <122161095+Lucasbc47@users.noreply.github.com>
2023-01-19 21:49:42 -0300

    Create Main.java

---
+++ 008/Main.java
@@ -0,0 +1,11 @@
+public class main {
+    public static void main(String[] args) {
+
+        int x = 1;
+        while (x < 10) {
+            if (x  < 3) {
+                System.out.println("Big x");
+                break;
+            }
+        }
+        
