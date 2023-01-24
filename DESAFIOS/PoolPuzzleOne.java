20baca2fae885257ea4b229a8a7a00cfab42812b
Lucas Barboza Costa <122161095+Lucasbc47@users.noreply.github.com>
2023-01-24 18:38:42 -0300

    Update PoolPuzzleOne.java

--- DESAFIOS/PoolPuzzleOne.java
+++ DESAFIOS/PoolPuzzleOne.java
@@ -1,32 +1,48 @@
-class PoolPuzzleOne {
+{
     public static void main(String args[]){
+
         int x = 0;
 
         while (x < 4){
-          
+            /*
+                a[empty]noise
+                annoys
+                an oyster
+             */
+
+            // Print (a)
             System.out.print("a");
-
             if(x < 1){
+                // Empty space []
                 System.out.print(" ");
-            }
-
-            System.out.print("n");
-
-            if (x > 1){
-                System.out.print(" oyster");
                 x = x + 2;
             }
-          
+
+            // Second time won't run since 2 > 1
+
+            // Print first [n]
+            // annoys
+            System.out.print("n");
+
+            // Won't run for first time
             if (x == 1){
+                System.out.print(" oyster");
+            }
+
+            // First time == [0]; won't run also
+            if (x > 1){
                 System.out.print("noys");
             }
-          
+
+            // Runs by second so, output (a noise)
             if (x < 1){
                 System.out.print("oise");
             }
-          
+
+            // now 0 == 2.
             System.out.println("");
             x = x + 1;
         }
     }
 }
+
