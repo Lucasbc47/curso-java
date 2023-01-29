8da806f0e85faa7fa99f5a8a9263d5e24aa9de3c
Lucas Barboza Costa <122161095+Lucasbc47@users.noreply.github.com>
2023-01-29 11:19:54 -0300

    Update exercicio_18.java

--- exs/exercicio_18.java
+++ exs/exercicio_18.java
@@ -1,10 +1,17 @@
 import java.util.Scanner;
 
 public class exercicio_18 {
-    public static void main(String[] args) {
+      public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Insira o numero para quebrar");
-        String numero = scanner.nextLine();
+        System.out.println("Insira o texto que deseja quebrar:");
+        String texto = scanner.nextLine();
 
+        for(int i = 0; i < texto.length(); i++){
+            System.out.print(texto.charAt(i) + " ");
+            // .charAt()
+            // é o metodo de String que retorna o carectere
+            // localizado.
+            // dentro dos parenteses, vc passa o numero/posição
+        }
     }
 }
