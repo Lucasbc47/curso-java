5a47faeae38bc08495bc6daf480e8c08d3a421ab
Lucas Barboza Costa <122161095+Lucasbc47@users.noreply.github.com>
2023-01-31 20:53:19 -0300

    Update exercicio_19.java

--- exs/exercicio_19.java
+++ exs/exercicio_19.java
@@ -6,10 +6,16 @@ public class exercicio_19{
 
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("First variable:");
+        System.out.println("Primeira variavel:");
         String first_content = scanner.nextLine();
-        System.out.println("Second variable:");
+
+        System.out.println("Segunda variavel:");
         String second_content = scanner.nextLine();
+
+        if (first_content.length() == 0 && second_content.length() == 0){
+            System.out.println("Favor, insira pelo menos uma palavra!");
+            System.exit(0);
+        }
 
         boolean t = first_content.contains(second_content);
         System.out.println(t);
